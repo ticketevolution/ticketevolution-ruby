a432dde00915caa91f2027d9cd4ab8fa79def707
Jonathan Clem <j@jclem.net>
2012-06-11 13:00:46 -0400

    to_ordered_hash sorts array values

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -3,6 +3,8 @@ class Hash
     ActiveSupport::OrderedHash.new.tap do |hash|
       self.sort_by{|k,v| k.to_s}.each do |k, v|
         hash[k] = v
+
+        v.sort_by! { |e| e.to_s } if v.is_a? Array
       end
     end
   end
