9c838e9de91330bdaccfc92d33efb30d00343762
Jonathan Clem <j@jclem.net>
2012-06-12 15:43:56 -0400

    Fixed for Ruby 1.8.7

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -2,9 +2,8 @@ class Hash
   def to_ordered_hash
     ActiveSupport::OrderedHash.new.tap do |hash|
       self.sort_by{|k,v| k.to_s}.each do |k, v|
+        v = v.sort_by { |e| e.to_s } if v.is_a? Array
         hash[k] = v
-
-        v.sort_by! { |e| e.to_s } if v.is_a? Array
       end
     end
   end
