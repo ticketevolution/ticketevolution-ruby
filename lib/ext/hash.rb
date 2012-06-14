d9d2d4c96618c3efc1a91499d05b645e4487f74c
Dan Matthews <dan@ticketevolution.com>
2012-06-14 11:06:17 -0400

    Revert "Fixed for Ruby 1.8.7"
    This reverts commit 9c838e9de91330bdaccfc92d33efb30d00343762.

--- lib/ext/hash.rb
+++ lib/ext/hash.rb
@@ -2,8 +2,9 @@ class Hash
   def to_ordered_hash
     ActiveSupport::OrderedHash.new.tap do |hash|
       self.sort_by{|k,v| k.to_s}.each do |k, v|
-        v = v.sort_by { |e| e.to_s } if v.is_a? Array
         hash[k] = v
+
+        v.sort_by! { |e| e.to_s } if v.is_a? Array
       end
     end
   end
