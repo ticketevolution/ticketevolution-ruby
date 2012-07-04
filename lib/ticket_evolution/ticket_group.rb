cee13adf675136ccf5df93fd28e9850db8cb31b5
Jonathan Clem <j@jclem.net>
2012-07-04 12:38:54 -0400

    TicketGroup#hold passes id to TicketGroups

--- lib/ticket_evolution/ticket_group.rb
+++ lib/ticket_evolution/ticket_group.rb
@@ -1,7 +1,7 @@
 module TicketEvolution
   class TicketGroup < Model
     def hold(params)
-      plural_class.new(:parent => @connection).hold(params)
+      plural_class.new(:parent => @connection, :id => self.id).hold(params)
     end
   end
 end
