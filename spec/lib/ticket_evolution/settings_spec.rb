53f1ecf0b875f4b585d4c03fcc03fd98bbf8c08d
JJ Mocko <devSWnoda@gmail.com>
2012-08-01 12:05:32 -0400

    Add tests for settings endpoint for service_fees action

--- spec/lib/ticket_evolution/settings_spec.rb
+++ spec/lib/ticket_evolution/settings_spec.rb
@@ -6,11 +6,23 @@ describe TicketEvolution::Settings do
 
   it_behaves_like 'a ticket_evolution endpoint class'
 
-  context "integration tests" do
+  context "shipping integration tests" do
     use_vcr_cassette "endpoints/settings", :record => :new_episodes
 
-    it "returns a list of results" do
+    it "returns a list of results for shipping action" do
       search_results = connection.settings.shipping()
+
+      search_results.each do |result|
+        result.should be_a TicketEvolution::Model
+      end
+    end
+  end
+
+  context "service_fees integration tests" do
+    use_vcr_cassette "endpoints/settings_service_fees", :record => :new_episodes
+
+    it "returns a list of results for service_fees action" do
+      search_results = connection.settings.service_fees()
 
       search_results.each do |result|
         result.should be_a TicketEvolution::Model
