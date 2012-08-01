fd4b095e11e7b0a7f81aa42334014a43c57cf042
JJ Mocko <devSWnoda@gmail.com>
2012-08-01 11:59:22 -0400

    Add ServiceFee to available models, and handle api_connection.settings.service_fees appropriately

--- lib/ticket_evolution/settings.rb
+++ lib/ticket_evolution/settings.rb
@@ -1,16 +1,33 @@
 module TicketEvolution
   class Settings < Endpoint
     def shipping(params = {})
-      request(:GET, "/shipping", params, &method(:build_for_search))
+      request(:GET, "/shipping", params, &method(:build_for_shipping))
     end
 
-    def build_for_search(response)
+    def build_for_shipping(response)
       collection = TicketEvolution::Collection.new(
         :total_entries => response.body['total_entries']
       )
 
       response.body['settings'].each do |result|
         collection.entries << "TicketEvolution::ShippingSetting".
+          constantize.new(result.merge({:connection => connection}))
+      end
+
+      collection
+    end
+
+    def service_fees(params = {})
+      request(:GET, "/service_fees", params, &method(:build_for_service_fees))
+    end
+
+    def build_for_service_fees(response)
+      collection = TicketEvolution::Collection.new(
+        :total_entries => response.body['total_entries']
+      )
+
+      response.body['settings'].each do |result|
+        collection.entries << "TicketEvolution::ServiceFee".
           constantize.new(result.merge({:connection => connection}))
       end
 
