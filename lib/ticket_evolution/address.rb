b920abd1577bd3d32ebc2a30a2b26cca9c24c6a1
Don Pflaster <don@ticketevolution.com>
2012-08-14 11:36:06 -0400

    Added fedex_check action to addresses endpoint

--- lib/ticket_evolution/address.rb
+++ lib/ticket_evolution/address.rb
@@ -1,4 +1,8 @@
 module TicketEvolution
   class Address < Model
+    def fedex_check
+      plural_class.new(:parent => @connection,:id => self.id).fedex_check
+    end
+
   end
 end
--- lib/ticket_evolution/clients/addresses.rb
+++ lib/ticket_evolution/clients/addresses.rb
@@ -6,6 +6,20 @@ module TicketEvolution
       include TicketEvolution::Modules::Show
       include TicketEvolution::Modules::Update
       include TicketEvolution::Modules::Destroy
+
+      def fedex_check(id, params=nil, &handler)
+        handler ||= method(:fedex_check_handler)
+        request(:GET, "/#{id}/fedex_check", nil, &handler)
+      end
+
+      def fedex_check_handler(response)
+        singular_class.new(response.body.merge({
+          :status_code => response.response_code,
+          :server_message => response.server_message,
+          :connection => response.body[:connection]
+        }))
+      end
+
     end
   end
 end
