3e8d4ea1097f6e8f494f4810c914a4a81c35932a
Don Pflaster <don@ticketevolution.com>
2012-08-17 16:52:01 -0400

    Added fedex_check_fields to address endpoint

--- lib/ticket_evolution/address.rb
+++ lib/ticket_evolution/address.rb
@@ -4,5 +4,9 @@ module TicketEvolution
       plural_class.new(:parent => @connection,:id => self.id).fedex_check
     end
 
+    def fedex_check_fields
+      plural_class.new(:parent => @connection).fedex_check_fields
+    end
+
   end
 end
--- lib/ticket_evolution/clients/addresses.rb
+++ lib/ticket_evolution/clients/addresses.rb
@@ -20,6 +20,20 @@ module TicketEvolution
         }))
       end
 
+      def fedex_check_fields(params=nil, &handler)
+        handler ||= method(:fedex_check_fields_handler)
+        params = { endpoint_name.to_sym => [params] } if params.present?
+        request(:POST, "/fedex_check_fields", params, &handler)
+      end
+
+      def fedex_check_fields_handler(response)
+        singular_class.new(response.body.merge({
+          :status_code => response.response_code,
+          :server_message => response.server_message,
+          :connection => response.body[:connection]
+        }))
+      end
+
     end
   end
 end
