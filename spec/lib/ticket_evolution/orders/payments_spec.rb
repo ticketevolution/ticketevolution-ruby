3c23eede70e57139a657b456fbf62db065dc17cd
Jonathan Clem <j@jclem.net>
2012-08-24 17:32:49 -0400

    Spec for Payments#show

--- spec/lib/ticket_evolution/orders/payments_spec.rb
+++ spec/lib/ticket_evolution/orders/payments_spec.rb
@@ -8,6 +8,7 @@ describe TicketEvolution::Orders::Payments do
   it_behaves_like 'a ticket_evolution endpoint class'
   it_behaves_like 'a create endpoint'
   it_behaves_like 'a list endpoint'
+  it_behaves_like 'a show endpoint'
   it_behaves_like 'an update endpoint'
 
   context 'integration tests' do
