7af6851ef483abb76e4866faed6a0197d9b22602
Ian <iangrabill@gmail.com>
2012-10-18 11:28:40 -0400

    Added tags endpoint to Brokerages

--- lib/ticket_evolution/brokerage.rb
+++ lib/ticket_evolution/brokerage.rb
@@ -1,4 +1,9 @@
 module TicketEvolution
   class Brokerage < Model
+
+    def tags(params)
+      plural_class.new(:parent => @connection,:id => self.id).tags(params)
+    end
+
   end
 end
--- lib/ticket_evolution/brokerages.rb
+++ lib/ticket_evolution/brokerages.rb
@@ -3,5 +3,17 @@ module TicketEvolution
     include TicketEvolution::Modules::List
     include TicketEvolution::Modules::Search
     include TicketEvolution::Modules::Show
+
+    def tags(params)
+      ensure_id
+      request(:GET, "/tags", params) do |response|
+        singular_class.new(response.body.merge({
+          :status_code => response.response_code,
+          :server_message => response.server_message,
+          :connection => response.body[:connection]
+        }))
+      end
+    end
+
   end
 end
