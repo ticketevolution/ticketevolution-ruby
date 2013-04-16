10b2525f1ad89d0f557e332cb2aa5b9fcf0ef506
Don Pflaster <dpflaster@gmail.com>
2013-04-16 18:28:03 -0400

    Added autocomplete to quotes

--- lib/ticket_evolution/quotes.rb
+++ lib/ticket_evolution/quotes.rb
@@ -5,5 +5,23 @@ module TicketEvolution
     include TicketEvolution::Modules::Show
     include TicketEvolution::Modules::Create
     include TicketEvolution::Modules::Update
+
+    def autocomplete(params = {})
+      request(:GET, "/autocomplete", params, &method(:build_for_search))
+    end
+
+    def build_for_search(response)
+      collection = TicketEvolution::Collection.new(
+        :total_entries => response.body['total_entries'],
+        :per_page => response.body['per_page'],
+        :current_page => response.body['current_page']
+      )
+
+      response.body['results'].each do |result|
+        collection.entries << result
+      end
+
+      collection
+    end
   end
 end
