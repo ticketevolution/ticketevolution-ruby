e4d1504ae141911f951177198eb6e499c6f4cf24
Robert Sosinski <email@robertsosinski.com>
2013-05-20 16:29:47 -0400

    gem fix

--- lib/ticket_evolution/reports.rb
+++ lib/ticket_evolution/reports.rb
@@ -1,11 +1,11 @@
 module TicketEvolution
   class Reports < Endpoint
     def sales(params = {})
-      request(:GET, '/reports/sales', params, &method(:build_for_reports))
+      request(:GET, '/sales', params, &method(:build_for_reports))
     end
 
     def inventory(params = {})
-      request(:GET, '/reports/inventory', params, &method(:build_for_reports))
+      request(:GET, '/inventory', params, &method(:build_for_reports))
     end
 
     def build_for_reports(response)
