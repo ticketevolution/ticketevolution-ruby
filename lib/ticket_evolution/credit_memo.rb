47e20977a9e6119b7771a05f663d8b505cebed20
Don Pflaster <dpflaster@gmail.com>
2013-05-23 17:38:38 -0400

    Added cards endpoint to credit memos

--- lib/ticket_evolution/credit_memo.rb
+++ lib/ticket_evolution/credit_memo.rb
@@ -1,5 +1,9 @@
 module TicketEvolution
   class CreditMemo < Model
     include Model::ParentalBehavior
+
+    def cards(params = nil)
+      plural_class.new(:parent => @connection,:id => self.id).cards(params)
+    end
   end
 end
--- lib/ticket_evolution/credit_memos.rb
+++ lib/ticket_evolution/credit_memos.rb
@@ -3,6 +3,18 @@ module TicketEvolution
     include TicketEvolution::Modules::List
     include TicketEvolution::Modules::Show
     include TicketEvolution::Modules::Update
+
+    def cards(params = nil)
+      ensure_id
+      request(:GET, "/cards", params) do |response|
+        singular_class.new(response.body.merge({
+          :status_code => response.response_code,
+          :server_message => response.server_message,
+          :connection => response.body[:connection]
+        }))
+      end
+    end
+
   end
 end
 
