bd8901cbf7843ff2ece23affd9216f1c4381328a
Rich Soni <rich@richsoni.com>
2014-03-03 09:47:38 -0500

    update to add me to ticketevolution-ruby

--- lib/ticket_evolution/credentials.rb
+++ lib/ticket_evolution/credentials.rb
@@ -1,5 +1,10 @@
 module TicketEvolution
   class Credentials < Endpoint
     include TicketEvolution::Modules::List
+
+    def me(params = {})
+      handler ||= method(:raw_handler)
+      request(:GET, '/me', params, &handler)
+    end
   end
 end
